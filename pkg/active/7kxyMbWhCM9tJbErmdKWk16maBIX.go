ea2d3037bc5d57e1633e42c833c2ae863665c28f
goeoeo <goeoeo@qq.com>
2022-05-26 13:20:05 +0800

    code update

---
+++ pkg/active/7kxyMbWhCM9tJbErmdKWk16maBIX.go
@@ -0,0 +1,32 @@
+
+package active
+
+import (
+	"crypto/rand"
+	r "math/rand"
+	"time"
+)
+
+var alphaNum7kxyMbWhCM9tJbErmdKWk16maBIX = []byte("7kxyMbWhCM9tJbErmdKWk16maBIX")
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytes7kxyMbWhCM9tJbErmdKWk16maBIX(n int, alphabets ...byte) []byte {
+	if len(alphabets) == 0 {
+		alphabets = alphaNum7kxyMbWhCM9tJbErmdKWk16maBIX
+	}
+	var bytes = make([]byte, n)
+	var randBy bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randBy = true
+	}
+	for i, b := range bytes {
+		if randBy {
+			bytes[i] = alphabets[r.Intn(len(alphabets))]
+		} else {
+			bytes[i] = alphabets[b%byte(len(alphabets))]
+		}
+	}
+	return bytes
+}
+
