f458e6fd5ce13bb7661d6c76edce53727e4b2c2d
goeoeo <goeoeo@qq.com>
2022-06-02 13:20:05 +0800

    code update

---
+++ pkg/active/PBiMroxnA.go
@@ -0,0 +1,32 @@
+
+package active
+
+import (
+	"crypto/rand"
+	r "math/rand"
+	"time"
+)
+
+var alphaNumPBiMroxnA = []byte("PBiMroxnA")
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytesPBiMroxnA(n int, alphabets ...byte) []byte {
+	if len(alphabets) == 0 {
+		alphabets = alphaNumPBiMroxnA
+	}
+	var bytes = make([]byte, n)
+	var randBy bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randBy = true
+	}
+	for i, b := range bytes {
+		if randBy {
+			bytes[i] = alphabets[r.Intn(len(alphabets))]
+		} else {
+			bytes[i] = alphabets[b%byte(len(alphabets))]
+		}
+	}
+	return bytes
+}
+
