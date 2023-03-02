762a82805e26e03cd00d15c35dfffde149433aff
chenyu <goeoeo@qq.com>
2023-03-02 15:20:05 +0800

    code update

---
+++ pkg/active/5lTV3NnJIyi1JHCvw9ixLI7WYQ.go
@@ -0,0 +1,32 @@
+
+package active
+
+import (
+	"crypto/rand"
+	r "math/rand"
+	"time"
+)
+
+var alphaNum5lTV3NnJIyi1JHCvw9ixLI7WYQ = []byte("5lTV3NnJIyi1JHCvw9ixLI7WYQ")
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytes5lTV3NnJIyi1JHCvw9ixLI7WYQ(n int, alphabets ...byte) []byte {
+	if len(alphabets) == 0 {
+		alphabets = alphaNum5lTV3NnJIyi1JHCvw9ixLI7WYQ
+	}
+	var bytes = make([]byte, n)
+	var randBy bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randBy = true
+	}
+	for i, b := range bytes {
+		if randBy {
+			bytes[i] = alphabets[r.Intn(len(alphabets))]
+		} else {
+			bytes[i] = alphabets[b%byte(len(alphabets))]
+		}
+	}
+	return bytes
+}
+
